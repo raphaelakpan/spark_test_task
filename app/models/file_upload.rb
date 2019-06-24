f2995e018901b028ed60318a096ca32ce59bbdf1
Raphael Akpan <raphael.akpan@andela.com>
2019-06-24 09:47:30 +0100

    Update file_upload model and add specs

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -12,32 +12,48 @@ class FileUpload < ApplicationRecord
     error: "error"
   }
 
-  def self.create_upload(file, creator)
+  def self.create_upload(file, creator_id)
     create!(
       file_name: file.original_filename,
       file_type: file.content_type,
-      creator: creator
+      creator_id: creator_id
     )
   end
 
-  def self.file_upload_for(id = nil, creator)
-    return where(creator: creator).last if id.nil?
-    where(id: id, creator: creator).first
+  def self.file_upload_for(id = nil, creator_id)
+    return where(creator_id: creator_id).last if id.nil?
+    where(id: id, creator_id: creator_id).first
   end
 
   def processing?
     state == STATES[:processing]
   end
 
+  def processing
+    update(state: STATES[:processing])
+  end
+
   def done?
     state == STATES[:done]
+  end
+
+  def done
+    update(state: STATES[:done])
   end
 
   def pending?
     state == STATES[:pending]
   end
 
+  def pending
+    update(state: STATES[:pending])
+  end
+
   def error?
     state == STATES[:error]
+  end
+
+  def error
+    update(state: STATES[:error])
   end
 end
